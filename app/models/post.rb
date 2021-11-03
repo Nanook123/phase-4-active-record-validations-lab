701fc81326be430447fcc365141080baba319c9c
Joshua Allford <joshallfordnanook@gmail.com>
2021-11-03 16:20:20 +0000

    commit tree 4e1973c0bee677e94519b0b2c82da9499d081007

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    # validate :title_bait
+    validate :clickbait?
+    
+
+    # def title_bait
+    #     if title == "True Facts"
+    #         errors.add(:title, "Only False News")
+    #     end
+    # end
+
+
+    
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+    
 end
